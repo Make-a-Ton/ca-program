d244639bed440fd691609bb5e8c021238947da9c
sunith-vs <sunith@eduport.app>
2024-09-17 23:42:27 +0530

    feat: add gunicorn

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,16 +8,6 @@ if [ "$DEBUG" = "True" ]; then
 else
   python3 manage.py collectstatic --noinput
 
-  # Run migrations
-
-  if [ "$DJANGO_SUPERUSER_EMAIL" ]
-  then
-      python manage.py createsuperuser \
-          --noinput \
-          --full_name $DJANGO_SUPERUSER_NAME \
-          --email $DJANGO_SUPERUSER_EMAIL
-  fi
-
-  gunicorn config.wsgi:application --bind 0.0.0.0:8000
+  gunicorn config.wsgi:application --bind 0.0.0.0:8000 --workers 3
 
 fi
