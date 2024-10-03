871f7d129e68cfdc9a964bfb2289a719794cf065
sunith-vs <sunith@eduport.app>
2024-10-04 00:33:13 +0530

    feat: update team

---
+++ base/management/commands/update_college.py
@@ -0,0 +1,32 @@
+import csv
+from django.core.management.base import BaseCommand
+from makeaton.models import TeamMember
+
+
+class Command(BaseCommand):
+    help = 'Update team member level from a CSV file based on the email.'
+
+    def add_arguments(self, parser):
+        # Argument to specify the path of the CSV file
+        parser.add_argument('csv_file', type=str, help='Path to the CSV file')
+
+    def handle(self, *args, **kwargs):
+        csv_file_path = kwargs['csv_file']
+
+        try:
+            with open(csv_file_path, newline='', encoding='utf-8') as csvfile:
+                reader = csv.DictReader(csvfile)
+
+                for row in reader:
+                    try:
+                        email = row['email']
+                        TeamMember.objects.filter(email=email).update(college_name=row['College Names'])
+                        self.stdout.write(self.style.SUCCESS(f"Updated {email}'s level to {row['College Names']}"))
+                    except Exception as e:
+                        self.stdout.write(self.style.ERROR(f"An error occurred: {email} {str(e)}"))
+
+        except FileNotFoundError:
+            self.stdout.write(self.style.ERROR(f"File {csv_file_path} not found"))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f"An error occurred: {str(e)}"))
+
